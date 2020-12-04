65e9e4ef61635f74f59c2a8a4f4c9465a1781ec0
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-12-03 21:38:06 -0300

    ajustes conexao

--- MODEL/conexao.php
+++ MODEL/conexao.php
@@ -1,9 +1,9 @@
 <?php
 	include_once("../VIEW/classeCabecalho.php");
 
-    $bd = "mysql:host=localhost:3306;dbname=sospet;charset=utf8";
-    $user = "root";
-    $senha = "";
+    $bd = "mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_bb3563937d246b4;charset=utf8";
+    $user = "ba3534f19130d4";
+    $senha = "9df3a70e";
 
     try{
     	$conexao = new PDO($bd,$user,$senha);
